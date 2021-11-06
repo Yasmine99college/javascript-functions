b72f6899591561db688509ed5a536b701cd313b6
Yasmine99college <yasminaahmedabcd@gmail.com>
2021-11-06 03:01:30 +0200

    Update gameoflife.js

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -7,7 +7,10 @@ function same([x, y], [j, k]) {
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  this.some((c)=>same(c,cell));
+
+}
 
 const printCell = (cell, state) => {};
 
